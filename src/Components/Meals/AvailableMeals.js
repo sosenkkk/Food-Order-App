bb153e47c2f7e0c97da837fd653de4fef8dbcc2d
Shashank Raj <shashankraj3636@gmail.com>
2023-01-11 23:55:03 +0530

    new after api

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,39 +1,32 @@
 import { Card } from "@mui/material";
 import styles from "./AvailableMeals.module.css"
 import MealItem from "./MealItem/MealItem";
-
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import { useEffect, useState } from "react";
 
 
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} describe={meal.description} price={meal.price} />);
+    useEffect(()=>{
+        const fetchMeals=async()=>{
+            const response= await fetch('https://zwiggy-dc710-default-rtdb.firebaseio.com/Meals.json');
+            const responseData= await response.json();
+            const loadedMeals=[];
+            for(const key in responseData){
+                loadedMeals.push({
+                    id:key,
+                    name:responseData[key].Name,
+                    description:responseData[key].description,
+                    price:responseData[key].price,
+                });
+            }
+            setmeals(loadedMeals);
+        };
+        fetchMeals();
+    }, []);
+
+    const [meals, setmeals] = useState([]);
+
+    const mealsList = meals.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} describe={meal.description} price={meal.price} />);
 
     return <section className={styles.meals} >
         <Card>
--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -6,7 +6,7 @@ import CartContext from "../../Store/cart-context";
 const MealItem = (props) => {
     const cartCtx = useContext(CartContext);
   
-    const price = `$${props.price.toFixed(2)}`;
+    const price = `₹${props.price.toFixed(2)}`;
   
     const addToCartHandler = (amount) => {
       cartCtx.addItem({

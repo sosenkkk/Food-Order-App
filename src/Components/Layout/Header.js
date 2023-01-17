8c594c013137e299c8d5ebd5653774c792fbd465
Shashank Raj <shashankraj3636@gmail.com>
2023-01-17 21:55:51 +0530

    added navbar

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Header.module.css";
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
     return <React.Fragment>
         <header className={styles.header} >
-            <a href="/"><button className={styles.logo}>Zwiggy</button></a>
-            <HeaderCartButton onCartClick={props.onCartClick} />
-       </header>
+            <div style={{textAlign:"center"}} >
+                <FastfoodIcon fontSize="large" /><button className={styles.logo}>Zwiggy</button>
+            </div>
+            <div>
+                <HeaderCartButton onCartClick={props.onCartClick} />
+            </div>
+        </header>
+
         <div className={styles['main-image']} >
             <img src="https://www.wallpaperflare.com/static/944/309/206/food-steak-wine-dessert-wallpaper.jpg" alt="food_image" />
         </div>
